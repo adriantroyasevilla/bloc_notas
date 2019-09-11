893ba058269c4bbc4ece4f45495e02d5ccc8f88c
unknown <atsvex94@gmail.com>
2019-09-11 12:36:46 +0200

    Nueva funcion añadir_nota

--- bloc_notas/bloc_notas.py
+++ bloc_notas/bloc_notas.py
@@ -19,12 +19,48 @@ import os
 def inicio():
     os.system("clear")
     menu()
+
     
 def menu():
-    print(f'    BIENVENIDO AL BLOC DE NOTAS DE ADRIÁN')
-    print('-'*50)
-    opcion = int(input(f'Selecciona la opción que desees realizar:\n\n Nueva nota[1]\n Ver notas[2]\n Borrar nota[3]\n Salir[4] -->'))
-    return opcion
+    while True:
+        print(f'    BIENVENIDO AL BLOC DE NOTAS DE ADRIÁN')
+        print('-'*50)
+        opcion = int(input(f'\nSelecciona la opción que desees realizar:\n\n Nueva nota[1]\n Ver notas[2]\n Borrar nota[3]\n Salir[4] --> '))
+        try:
+            if opcion == 1:
+                añadir_nota()
+        # elif opcion == 2:
+        #     def ver_notas()
+        # elif opcion == 3:
+        #     def borrar_nota()
+            elif opcion == 4:
+                exit()
+            else:
+                'Escoge una opción correcta'
+        except ValueError:
+                'Debes introducir un número'
 
+def añadir_nota():
+    nota = input(f'\nIntroduce la nota: ')
+    print('-- NOTA GUARDADA --\n')
+    bloc = open('bloc.csv', 'a')
+    bloc.write(f'{nota}\n')
+    bloc.close()
+
+# def opcion_seleccionada(opcion):
+#     try:
+#         if opcion == 1:
+#             añadir_nota()
+#     elif opcion == 2:
+#         def ver_notas()
+#     elif opcion == 3:
+#         def borrar_nota()
+#         elif opcion == 4:
+#             exit()
+#         else:
+#             'Escoge una opción correcta'
+#     except ValueError:
+#             'Debes introducir un número'
+    
 if __name__ == "__main__":
     inicio()
